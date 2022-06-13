dbb26d9b5148a81d035cad09b9aa68cf80b4bb3d
T Omoto <tyler.omoto@gmail.com>
2022-06-13 01:23:29 -0400

    added in prompts for each class

--- index.js
+++ index.js
@@ -1,2 +1,95 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+teamArray = [];
+
+// Function to start the prompt asking which team member
+// Switch case for each option 
+// Build HTML 
+
+function newTeam(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeTypePrompt",
+            message: "What type of employee are you adding to the team?",
+            choices: ["Manager", "Engineer", "Intern", "None"]
+        }
+    ])
+}
+
+// Function to prompt manager questions when selected from newTeam Prompt
+function newManager() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the manager's id number?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's email?"
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is the manager's office number?"
+        }
+    ])
+}
+// Function to prompt engineer questions when selected from newTeam Prompt
+function newEngineer(){
+    inquirer.prompt([
+        { 
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's id number?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's github username?"
+        }
+    ])
+}
+// Function to prompt intern questions when selected from newTeam Prompt
+function newIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the intern's id number?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email?"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Where does the intern attend school?"
+        }
+    ])
+}
